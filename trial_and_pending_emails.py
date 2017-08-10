95dcbaa957794ce0a7c133a3845406d3ad3ab7ce
Robert Stefanic <rstefanic72@gmail.com>
2017-08-09 22:27:27 -0500

    added club class

--- trial_and_pending_emails.py
+++ trial_and_pending_emails.py
@@ -1,10 +1,14 @@
-from exchangelib import Account, Configuration, DELEGATE, Mailbox, Message, ServiceAccount
+from exchangelib import Account, Configuration, DELEGATE, HTMLBody, \
+                        Mailbox, Message, ServiceAccount
 from EmailSettings import EmailSettings
 
 import datetime
 import json
 import os
 import sys
+
+PENDING_CLUBS = []
+TRIAL_CLUBS = []
 
 TRIAL_EMAIL = os.path.join(os.path.abspath('.'), 'trial.html')
 PENDING_EMAIL = os.path.join(os.path.abspath('.'), 'pending.html')
@@ -32,20 +36,44 @@ class CEEmailAccount(object):
     def show_inbox_count(self):
         return self.account.inbox.total_count
 
-"""
-addr = ''
-msg = Message(
-    account=this.account,
-    folder=account.sent,
-    subject='Test Email',
-    body='Test Body for email',
-    to_recipients=[Mailbox(email_address=addr)]
-    )
+    def send_email(self, trial, admin_name, to_address):
 
-msg.send_and_save()
-print("Done.")
-"""
+        # Construct body of message
+        body = "Dear {}".format(admin_name)
+        if trial:
+            with open(TRIAL_EMAIL) as file:
+                body = ''.join(file.readlines())
+        else:
+            with open(PENDING_EMAIL) as file:
+                body = ''.join(file.readlines())
 
+        # Build and send message
+        msg = Message(
+            account=this.account,
+            folder=account.sent,
+            subject='Test Email',
+            body= HTMLBody(body),
+            to_recipients=[Mailbox(email_address=to_address)]
+        )
+
+        msg.send_and_save()
+        print("Message to {} sent.".format(admin_name))
+
+class Club(object):
+
+    def __init__(self):
+        self.club_name = input("Club name: ").strip()
+        self.club_id = input("Club ID: ").strip()
+        self.email = input("Email: ").strip()
+        self.admin_name = input("Admin name: ").strip()
+        trial = input("Trial Account [Y/n]: ").lower()
+
+        if trial == 'n':
+            self.trial = False
+        else:
+            self.trial = True
+
+            
 def main():
     email_settings = EmailSettings()
     print("Connecting...")
@@ -54,7 +82,6 @@ def main():
                                    email_settings.smtp_address,
                                    email_settings.server)
                    
-
 #   print("\nLook for clubs with an expiration date of {:%m-%d-%Y} or earlier.\n"
 #          .format(datetime.datetime.now() + datetime.timedelta(days=14)))
     print(email_account.show_inbox_count())
